92deb4d40a8aae03e51d5e086573453c7c9c847d
Edoardo Conti <c_edoardo@icloud.com>
2022-06-14 19:38:56 +0200

    scriptjutsu
    - macOS script updated
    - Linux x64 script added

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,48 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-SCRIPT_PATH="$SCRIPT_DIR/script-utils/ctypesloader.py"
+shopt -s xpg_echo
 
 # go to home directory
 cd $HOME
 
-# update brew (install if needed)
-echo "${bold}> checking homebrew installation...${normal}"
-which -s brew
-if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "  ${bold}homebrew installed${normal}"
+# install pyenv
+echo "${bold}> installing pyenv...${normal}"
+if ! command -v pyenv &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \ 
+    git ffmpeg
+    if curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash ; then
+        #echo "  ${bold}pyenv installed! please, run this script again to continue${normal}"
+        #exec bash
+        . $HOME/.bashrc
+        echo "  ${bold}ok${normal}"
+    else
+        echo "  ${bold}failed${normal}"
+        exit 1
+    fi
 else
-    brew update -q
-    echo "  ${bold}ok${normal}"
+    echo "  ${bold}skip${normal}"
 fi
 
-# install pyenv
-echo "\n${bold}> installing brew packages (git, wget, pyenv)...${normal}"
-if brew install git wget pyenv ; then
-    echo "  ${bold}ok${normal}"
-else
-    echo "  ${bold}failed${normal}"
-    exit 1
-fi
 
 # install python 3.7.13 if not present and set it as default
 echo "\n${bold}> installing python version 3.7.13 (required by voca)${normal}"
-if ! grep -q 'eval "$(pyenv init -)"' '.zshrc' ; then
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-    . $HOME/.zshrc
+if ! grep -q 'eval "$(pyenv init -)"' '.bashrc' ; then
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    . $HOME/.bashrc
 fi
 
 if
     pyenv install 3.7.13 -s &&
     pyenv local 3.7.13 &&
-    . $HOME/.zshrc
+    . $HOME/.bashrc
 then
     echo "  ${bold}ok${normal}"
 else
@@ -63,16 +64,15 @@ echo "  ${bold}ok${normal}"
 echo "\n${bold}> installing pip modules...${normal}"
 pip install wget numpy scipy chumpy opencv-python resampy python-speech-features tensorflow==1.15.2 scikit-learn image ipython matplotlib trimesh pyrender
 pip install --upgrade protobuf==3.20.0
-cp $SCRIPT_PATH .virtualenvs/vocablender/lib/python3.7/site-packages/OpenGL/platform/ctypesloader.py
 echo "  ${bold}ok${normal}"
 
 # install mesh lib
 echo "\n${bold}> installing psbody-mesh...${normal}"
-pip install https://github.com/MPI-IS/mesh/releases/download/v0.4/psbody_mesh-0.4-cp37-cp37m-macosx_10_9_x86_64.whl
+pip install https://github.com/MPI-IS/mesh/releases/download/v0.4/psbody_mesh-0.4-cp37-cp37m-linux_x86_64.whl
 echo "  ${bold}ok${normal}"
 
 # reset the python system version
 pyenv local system
-. $HOME/.zshrc
+. $HOME/.bashrc
 
 echo "\n${bold}> blender voca addon dependencies installed successfully!${normal}"
